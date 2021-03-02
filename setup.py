e4b3304b59f72836da04535e87be09a149dba5a8
Rachel Hassall <rachel.hassall@uk.nationalgrid.com>
2021-03-02 15:28:35 +0000

    FEAT: initial commit, with cookie cutter structure

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='technical report for chartered engineer application',
+    author='Rachel Hassall',
+    license='BSD-3',
+)
