cb3c6c49f2b4327c84134f7463b40968442cc27f
Rachel Hassall <rachel.hassall@uk.nationalgrid.com>
2021-03-19 11:55:12 +0000

    TESTS: create_raw_data test for electricity actuals

--- tests/test_create_raw_data.py
+++ tests/test_create_raw_data.py
@@ -2,8 +2,10 @@ import pytest
 from pandas.testing import assert_frame_equal, assert_series_equal
 
 import pandas as pd
+import os
 
 from src.data.create_raw_data import get_electricity_actuals
+
 
 def test_get_electricity_actuals_basic(mocker):
     """
@@ -17,35 +19,40 @@ def test_get_electricity_actuals_basic(mocker):
 
     mocker.patch("requests.get", mock_get_data)
 
-    actuals = get_electricity_actuals("2020-01-01", "2020-01-01")
+    actuals = get_electricity_actuals(
+        "2020-01-01", "2020-01-01", os.environ.get("elexon_api_key"), None
+    )
 
-    desired = pd.DataFrame({
-        "ELEC_DAY" : pd.to_datetime(["2020-01-01", "2020-01-01"]),
-        "SETTLEMENT_PERIOD" : [1,2]})
+    desired = pd.DataFrame(
+        {
+            "ELEC_DAY": pd.to_datetime(["2020-01-01", "2020-01-01"]),
+            "SETTLEMENT_PERIOD": [1, 2],
+        }
+    )
 
     desired["RECORDTYPE"] = "FUELHH"
     columns = [
-                        "CCGT",
-                        "OIL",
-                        "COAL",
-                        "NUCLEAR",
-                        "WIND",
-                        "PUMPEDSTORAGE",
-                        "NPSHYD",
-                        "OCGT",
-                        "OTHER",
-                        "INTFR",
-                        "INTIRL",
-                        "INTNED",
-                        "INTEW",
-                        "BIOMASS",
-                        "INTNEM",
-                        "INTELEC",
-                        "INTIFA2",
-                        "INTNSL",
-        ]
+        "CCGT",
+        "OIL",
+        "COAL",
+        "NUCLEAR",
+        "WIND",
+        "PUMPEDSTORAGE",
+        "NPSHYD",
+        "OCGT",
+        "OTHER",
+        "INTFR",
+        "INTIRL",
+        "INTNED",
+        "INTEW",
+        "BIOMASS",
+        "INTNEM",
+        "INTELEC",
+        "INTIFA2",
+        "INTNSL",
+    ]
 
     for col in columns:
         desired[col] = 10
 
-    assert_frame_equal(desired, demand)
+    assert_frame_equal(desired, actuals)
